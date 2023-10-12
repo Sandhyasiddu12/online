e78e7d3f78c9fca96c3133fcfc8eebbc8d310de6
sandhyasiddu0405@gmail.com <sandhyasiddu0405@gmail.com>
2023-10-12 14:42:09 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +11,38 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def submit_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    name =  self.name.text
+    email = self.email.text
+    alert('submitted')
+
+    anvil.server.call('add_register', name, email)
+
+    self.clear_all()
+  def clear_all(self):
+   self.name.text=''
+   self.email.text=''
+
+  def name_show(self, **event_args):
+    """This method is called when the TextBox is shown on the screen"""
+    pass
+
+  def student_name_show(self, **event_args):
+    """This method is called when the Label is shown on the screen"""
+    pass
+
+  def student_email_show(self, **event_args):
+    """This method is called when the Label is shown on the screen"""
+    pass
+
+
+
+   
+   
+
+
+  
+    
+
